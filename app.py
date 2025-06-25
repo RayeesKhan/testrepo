7f95243e7083e9d71d40c6dc3bc202738e0bb73c
RayeesKhan <rayeeskhan789@gmail.com>
2025-06-25 20:45:36 -0300

    cm

---
+++ app.py
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My First Web Page</title>
+</head>
+<body>
+    <h1>Welcome to My Website</h1>
+    <p>This is a simple HTML example. thanks ou can add more content as you learn!</p>
+    <a href="https://www.example.com" target="_blank">Visit Example Site</a>
+</body>
+</html>
+
---
+++ app2.py
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple Image Example</title>
+</head>
+<body>
+    <h2>My Favorite Image</h2>
+    <img src="https://via.placeholder.com/300" alt="Placeholder Image" width="300" height="200">
+</body>
+</html>
+
